e18d8396372dad063445257dce6576c8558e8f36
paolobighignoli <paolo.bighignoli@yeldo.com>
2022-11-06 22:31:04 +0100

    python project structure

--- project/entities/classes/abstract_class.py
+++ project/entities/classes/abstract_class.py
@@ -5,8 +5,7 @@ from typing import *
 class AbstractClass(ABC):
 
     @abstractmethod
-    def first_method(self, arg_1: float, arg_2: float,
-                         arg_3: List[int]) -> List[str]:
+    def first_method(self, arg_1: float, arg_3: List[int]) -> List[str]:
         """
         Description of first_method
         """
